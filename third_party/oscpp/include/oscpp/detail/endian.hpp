8ab56c250f7395914a950796509c2aec1708593a
Parker Riebs <prariebs@gmail.com>
2020-12-20 18:23:19 -0600

    Add arm little endian to oscpp library

--- third_party/oscpp/include/oscpp/detail/endian.hpp
+++ third_party/oscpp/include/oscpp/detail/endian.hpp
@@ -51,27 +51,11 @@
 #        error Unknown machine endianness detected.
 #    endif
 #    define OSCPP_BYTE_ORDER __BYTE_ORDER
-#elif defined(_BIG_ENDIAN) && !defined(_LITTLE_ENDIAN) ||     \
-    defined(__BIG_ENDIAN__) && !defined(__LITTLE_ENDIAN__) || \
-    defined(_STLP_BIG_ENDIAN) && !defined(_STLP_LITTLE_ENDIAN)
-#    define OSCPP_BIG_ENDIAN
-#    define OSCPP_BYTE_ORDER OSCPP_BYTE_ORDER_BIG_ENDIAN
-#elif defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN) ||     \
-    defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__) || \
-    defined(_STLP_LITTLE_ENDIAN) && !defined(_STLP_BIG_ENDIAN)
-#    define OSCPP_LITTLE_ENDIAN
-#    define OSCPP_BYTE_ORDER OSCPP_BYTE_ORDER_LITTLE_ENDIAN
-#elif defined(__sparc) || defined(__sparc__) || defined(_POWER) || \
-    defined(__powerpc__) || defined(__ppc__) || defined(__hpux) || \
-    defined(__hppa) || defined(_MIPSEB) || defined(_POWER) ||      \
-    defined(__s390__)
-#    define OSCPP_BIG_ENDIAN
-#    define OSCPP_BYTE_ORDER OSCPP_BYTE_ORDER_BIG_ENDIAN
 #elif defined(__i386__) || defined(__alpha__) || defined(__ia64) ||  \
     defined(__ia64__) || defined(_M_IX86) || defined(_M_IA64) ||     \
     defined(_M_ALPHA) || defined(__amd64) || defined(__amd64__) ||   \
     defined(_M_AMD64) || defined(__x86_64) || defined(__x86_64__) || \
-    defined(_M_X64) || defined(__bfin__)
+    defined(_M_X64) || defined(__bfin__) || defined(__arm__)
 
 #    define OSCPP_LITTLE_ENDIAN
 #    define OSCPP_BYTE_ORDER OSCPP_BYTE_ORDER_LITTLE_ENDIAN
